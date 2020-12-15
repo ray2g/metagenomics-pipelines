94c0ba601b2d79a81b0fe3506e98c5bb40f7193b
João Raimundo <32943125+ray2g@users.noreply.github.com>
2020-12-15 15:55:18 +0000

    Update DADA2_directory_setup_db_downloader.sh

--- DADA2_directory_setup_db_downloader.sh
+++ DADA2_directory_setup_db_downloader.sh
@@ -2,8 +2,11 @@
 ## João Raimundo @BioData.pt
 
 # directory setup for DADA2 pipeline
-mkdir <analysisName> <analysisName>/tables <analysisName>/plots <analysisName>/data <analysisName>/taxonomy <analysisName>/taxonomy/database <analysisName>/taxonomy/objects
+mkdir <analysisName> <analysisName>/tables <analysisName>/plots <analysisName>/data \
+<analysisName>/taxonomy <analysisName>/taxonomy/database <analysisName>/taxonomy/objects
 
 # download SILVA v138 database: train set & species
 wget https://zenodo.org/record/3986799/files/silva_nr99_v138_train_set.fa.gz?download=1 -O <analysisName>/taxonomy/database/silva_nr99_v138_train_set.fa.gz
 wget https://zenodo.org/record/3986799/files/silva_species_assignment_v138.fa.gz?download=1 -O <analysisName>/taxonomy/database/silva_species_assignment_v138.fa.gz
+
+
