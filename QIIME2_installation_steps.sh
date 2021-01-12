7306de5bbbb9d9bb3e84490ba76f53c4586dc8f6
João Raimundo <32943125+ray2g@users.noreply.github.com>
2021-01-12 16:02:35 +0000

    Update QIIME2_installation_steps.sh

--- QIIME2_installation_steps.sh
+++ QIIME2_installation_steps.sh
@@ -10,12 +10,9 @@ $ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh # d
 $ chmod +x Miniconda3-latest-Linux-x86_64.sh # define as executable
 $ ./Miniconda3-latest-Linux-x86_64.sh -y # installation
 
-# Create a new conda environment
-$ sudo apt-get update && apt-get upgrade -y
-$ conda create --name qiime2-2020.11 -y # in this example we choose "qiime_2020.11" as the name.
-$ conda activate qiime2-2020.11 # activate the environment
-
-# QIIME2 Installation
-$ conda install -c qiime2/label/r2020.11 qiime2 -y # installing qiime2 release 2020.11 in the new environment
-
+# QIIME2 Installation with Conda
+$ wget https://data.qiime2.org/distro/core/qiime2-2020.11-py36-linux-conda.yml
+conda env create -n qiime2 --file qiime2-2020.11-py36-linux-conda.yml
+# OPTIONAL CLEANUP
+$ rm qiime2-2020.11-py36-linux-conda.yml
 
